be0ae63cf61beb03bead7b16d1bd462d04318788
AIDAN <dr.hammer1234@gmail.com>
2023-03-22 13:43:03 -0700

    change html with :. added javascript so the number entered by the user is added to the page.

--- java/javascript.js
+++ java/javascript.js
@@ -20,27 +20,35 @@
 // }
 
 // greet('neda')
-let input = prompt('enter the radius of a circle')
+// let input = prompt('enter the radius of a circle')
 
-function circle_area(input){
-    const piy = ((input**2) * Math.PI);
-    console.log(`the area with the radius ${input} is ${piy}`)
-}
+// function circle_area(input){
+//     const piy = ((input**2) * Math.PI);
+//     console.log(`the area with the radius ${input} is ${piy}`)
+// }
 
-circle_area(input);
+// circle_area(input);
 
 "neda version"
+const radiusPara = document.querySelector('#radius');
+console.log(radiusPara);
+const resultPara = document.querySelector('#result');
+console.log(resultPara);
 
 function calculateRadius(radias){
     if (isNaN(radias)){
-        alert('this is not a number')
+        // alert('this is not a number');
+        resultPara.textContent = ('you entered a non number')
     }else {
         const area = Math.PI * radias ** 2;
-        return area;
+        return area;    
     }
 }
 let userValue = prompt('enter a number');
-let result = calculateRadius(userValue)
+let result = calculateRadius(userValue);
+radiusPara.textContent += userValue;
+
+
 if (result !== undefined) {
-    alert('this is not a number')
+    alert(`the area with hte circle with radius ${uservalue} is ${result.toFixed(2)}`)
 }
