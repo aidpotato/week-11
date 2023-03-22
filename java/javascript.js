1417c743fc2295d50158e5b3c6abdefc4fb6e86d
AIDAN <dr.hammer1234@gmail.com>
2023-03-22 12:36:00 -0700

    made function that has a set value for the time

--- java/javascript.js
+++ java/javascript.js
@@ -1,3 +1,22 @@
-function greet(a){
-    console.log(`hello ${a}`);
+function greet(username, time = 'day'){
+    console.log(`good ${time} ${username}`);
 }
+
+greet('bob')
+
+// const greet = function (name)
+
+// {
+//     console.log(`hello ${name}`);
+// }
+
+// greet('neda')
+
+
+// const greet = (name) =>
+
+// {
+//     console.log(`hello ${name}`);
+// }
+
+// greet('neda')
